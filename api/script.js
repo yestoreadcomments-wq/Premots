6d12e145cd02ca8243911502019280748bb00b71
yestoreadcomments-wq <yestoreadcomments@gmail.com>
2026-01-13 15:54:27 +0600

    Update script.js

--- api/script.js
+++ api/script.js
@@ -1,7 +1,5 @@
+
 export default function handler(req, res) {
   res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(`
-print("hello from github + vercel")
-print("this script is hosted remotely")
-  `);
+  res.status(200).send('print("hello from vercel")');
 }
